d7cb44d655e56db3402db9b4a4424f4ceaa84f64
Khenz <khlarenzdavid@gmail.com>
2026-01-19 18:57:25 +0800

    Update working experience section with highlighted roles, company names, and dates

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ var watchBtns = document.querySelectorAll(".watch-btn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
+// Ensure modal is hidden on page load
+if (modal) {
+    modal.style.display = "none";
+}
+
 // Function to open modal with image
 function openImageModal(imageSrc) {
     var modalContentArea = document.getElementById("modal-content-area");
@@ -19,7 +24,7 @@ function openImageModal(imageSrc) {
 function openYoutubeModal(youtubeUrl) {
    var modalContentArea = document.getElementById("modal-content-area");
     modalContentArea.innerHTML = `
-        <iframe width="560" height="315" src="${youtubeUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe src="${youtubeUrl}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
     `;
     modal.style.display = "flex"; // Changed from "block" to "flex"
 }
@@ -39,8 +44,10 @@ watchBtns.forEach(function(btn) {
 });
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
+if (span) {
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
